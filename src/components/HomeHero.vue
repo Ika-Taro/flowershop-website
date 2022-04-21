d85074b71717b06d5f4d99929aad0f129f96bf91
Ika-Taro <black.jkh@gmail.com>
2022-04-21 21:54:00 +0900

    カルーセル実装完了

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="home-hero">
+    <v-container fluid fill-height class="home-hero__content">
+        <div class="hero-logo">
+          <v-img class="hero-logo-img"
+          src= "../assets/flowershoplogo.jpg"
+          height="200"
+          width="200"
+          ></v-img>
+        </div>
+    </v-container>
+  </section>
+</template>
+<style  lang="scss" scoped>
+.home-hero__content {
+  background: url("../assets/flowershop-hero1.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+.hero-logo {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+}
+</style>
---
+++ src/components/ShopWorks.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container class="img-h"><v-carousel
+  >
+    
+      <div class="carousel-item">
+        <v-carousel-item 
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </div>
+    
+  </v-carousel></v-container>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: require('@/assets/flowershop-slide1.jpg'),
+          },
+          {
+            src: require('@/assets/flowershop-slide2.jpg'),
+          },
+          {
+            src: require('@/assets/flowershop-slide3.jpg'),
+          },
+          {
+            src: require('@/assets/flowershop-slide4.jpg'),
+          },
+        ],
+      }
+    },
+  }
+</script>
+
+<style  lang="scss" scoped>
+  .carousel-item {
+    object-fit: cover;
+  }
+</style>
